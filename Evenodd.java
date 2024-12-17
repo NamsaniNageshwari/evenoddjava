e184260c04e90bb89540e9d891ab1039d59e54a3
Nageshwari <namsanikishan77@gmail.com>
2024-12-17 14:20:45 +0530

    Java program to find given number is even or odd

---
+++ Evenodd.java
@@ -0,0 +1,10 @@
+public class Evenodd{
+	public static void main(Sting[] args){
+		int num=9;
+		if(num%2==0)
+			System.out.println(num+"is EVEN");
+		else
+			System.out.println(num+"is ODD");
+	}
+}
+
